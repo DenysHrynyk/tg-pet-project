750dc4eda8e598f224e725e438d328273d5d961f
DenysHrynyk <krill920@gmail.com>
2022-10-25 19:43:02 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-const tg = window.Telegram.WebApp
 function App() {
 
     useEffect(() => {
         tg.ready()
     }, [] )
-    const onClose = () => {
-        tg.close()
-    }
+
   return (
     <div className="App">
 work
