1e7025fa50aa8eb41ae2b56e120b84dd7ddf8244
DenysHrynyk <krill920@gmail.com>
2022-10-26 18:22:37 +0300

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -15,13 +15,13 @@ const Form = () => {
             subject
         }
         tg.sendData(JSON.stringify(data))
-    }, [])
+    }, [country, street, subject])
     useEffect(() => {
         tg.onEvent('mainButtonClicked', onSendData)
         return () => {
            tg.offEvent('mainButtonClicked', onSendData)
         }
-    })
+    }, [onSendData])
 
     useEffect(() => {
         tg.MainButton.setParams({
